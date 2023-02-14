54d7e06f8167212c44786b0857c225555a3021a9
Raghunath javare <raghunath.javare@gmail.com>
2023-02-14 18:31:21 +0530

    add overlay to black

--- script/script.js
+++ script/script.js
@@ -1,8 +1,13 @@
 const displayPic = document.querySelectorAll(".box");
-const resize = document.getElementById("resize");
+const overlay = document.querySelector(".overlay");
 for (let i = 0; i < displayPic.length; i++)
   displayPic[i].addEventListener("click", function () {
-    displayPic[i].style = "width:100%;";
-    resize.addEventListener("click", function () {
+    displayPic[i].style = "width:97%;";
+
+    overlay.classList.remove("hidden");
+
+    overlay.addEventListener("click", function () {
+      overlay.classList.add("hidden");
+      displayPic[i].style = "";
     });
   });
