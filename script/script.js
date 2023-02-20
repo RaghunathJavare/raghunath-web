09ee7886412a219bdb795f9e8409e964f26679c1
Raghunath javare <raghunath.javare@gmail.com>
2023-02-20 19:46:24 +0530

    add all files

--- script/script.js
+++ script/script.js
@@ -9,20 +9,19 @@ const pageHidden=document.querySelector('.hide-1');
 
 const pageOpen=document.querySelector('.hide-2');
 
-
+const photoLink=document.querySelector('.expand-photo');
+console.log(photoLink);
 
 // section background color hide 
 
 const overlay = document.querySelector(".overlay");
 for (let i = 0; i < displayPic.length; i++)
   displayPic[i].addEventListener("click", function () {
-    displayPic[i].style = "width:97%;";
-
-    overlay.classList.remove("hidden");
-
-    overlay.addEventListener("click", function () {
-      overlay.classList.add("hidden");
-      displayPic[i].style = "";
+      
+      overlay.classList.remove("hidden");
+      
+      overlay.addEventListener("click", function () {
+          overlay.classList.add("hidden");
     });
   });
 
