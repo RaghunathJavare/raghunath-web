41a4653208049959651fb90eb812c65d74a1eef6
Raghunath javare <raghunath.javare@gmail.com>
2023-03-12 19:37:23 +0530

    add a lick to my phone number

--- scripts/script.js
+++ scripts/script.js
@@ -4,17 +4,24 @@
 const btnHelp = document.querySelector(".btn-help");
 const btnBack = document.querySelector(".btn-back");
 const imgDice = document.querySelector(".game-img");
+const btnDownload = document.querySelector(".btn-download");
+const sectionGame = document.getElementById("game-section");
+console.log(sectionGame);
+console.log(btnDownload);
 
-btnBack.classList.add("hidden");
-console.log(btnHelp);
-
-btnHelp.addEventListener("click", function () {
-  imgDice.src = "images/background-1.jpg";
-  btnBack.classList.remove("hidden");
+btnDownload.addEventListener("click", function () {
+  errorMassage.classList.add("error");
 });
 
-btnBack.addEventListener("click", function () {
-  imgDice.src = "images/my-game-img.png";
-  btnBack.classList.add("hidden");
-});
+// btnBack.classList.add("hidden");
+// console.log(btnHelp);
 
+// btnHelp.addEventListener("click", function () {
+//   imgDice.src = "images/background-1.jpg";
+//   btnBack.classList.remove("hidden");
+// });
+
+// btnBack.addEventListener("click", function () {
+//   imgDice.src = "images/my-game-img.png";
+//   btnBack.classList.add("hidden");
+// });
