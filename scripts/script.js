dfa965e2e88799e7f8d4a5dac1505959ecbfcb96
Dhiraj jaware <dhiraj.javare@gmail.com>
2023-03-19 16:32:22 +0530

    update animation to profile

--- scripts/script.js
+++ scripts/script.js
@@ -2,10 +2,16 @@
 
 // Select Elements
 const btnDownload = document.querySelector(".btn-download");
+const heading = document.querySelector("h1");
 
+setInterval(function () {
+  heading.style.color = "yellow";
+}, 500);
+
+setInterval(function () {
+  heading.style.color = "#fff";
+}, 4000);
 
 btnDownload.addEventListener("click", function () {
   errorMassage.classList.add("error");
 });
-
-
