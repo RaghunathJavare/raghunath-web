85445239e1fe9bf5013d6ae7cb7e412f99894b2f
Raghunath javare <raghunath.javare@gmail.com>
2023-05-26 10:20:06 +0530

    add a reavel element funtionality to my project

--- scripts/script.js
+++ scripts/script.js
@@ -9,14 +9,34 @@ const eventHandler = function (e) {
   if (e.target.classList.contains("nav-link")) {
     const target = e.target;
     const link = target.closest(".navbar").querySelectorAll(".nav-link");
-    const home = target.closest(".navbar").querySelector(".home");
     if (!link) return;
     link.forEach((l) => {
       if (l !== target) l.style.opacity = this;
-      home.style.opacity=this
     });
   }
 };
 
 nav.addEventListener("mouseover", eventHandler.bind(0.5));
 nav.addEventListener("mouseout", eventHandler.bind(1));
+
+// Reval Elements
+
+const sections = document.querySelectorAll(".section");
+
+const revealSect = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section__hidden')
+  observer.unobserve(entry.target)
+ 
+};
+
+const observer = new IntersectionObserver(revealSect, {
+  root: null,
+  threshold: 0.15,
+});
+
+sections.forEach((section) => {
+  observer.observe(section);
+  section.classList.add('section__hidden')
+});
